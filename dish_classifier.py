4ce9f5dae5b68a2ea6f73d386a6175f7d4a6f53d
Asim Waheed <asim.waheed29@gmail.com>
2018-07-11 17:15:53 +0500

    Add Train/Test Split
    Currently shuffling the data and then splitting it while keeping 10% of the data for testing. Will decide later to stick with this or use GridSearchCV

--- dish_classifier.py
+++ dish_classifier.py
@@ -3,6 +3,8 @@ import pandas as pd
 from time import time 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.utils import shuffle
+from sklearn import model_selection	
+from sklearn.svm import SVC
 
 filename = "test.csv"
 
@@ -14,7 +16,6 @@ def ParseAndShuffleData(filename):
 	for index, row in df.iterrows():
 		row['Category'] = row['Category'].lower()
 		size = len(row['Category'])
-
 	# Deal with spaces at beginning or end
 		if row['Category'][0] == ' ':
 			row['Category'] = row['Category'][1:size]
@@ -34,16 +35,27 @@ def ParseAndShuffleData(filename):
 	return dishes, labels
 	# d_cat.to_csv("categories.csv")
 
+def PreProcess(features, labels):
+	dishes_train, dishes_test, labels_train, labels_test = model_selection.train_test_split(features, labels, test_size = 0.1, random_state = 1)
+	t0 = time()	
+	vect = CountVectorizer()
+	dishes_train = vect.fit_transform(dishes_train)
+	dishes_test = vect.transform(dishes_test)
+	print "vectorize time:", round(time() - t0, 3), "s"
+	print dishes_train.shape
+	print dishes_test.shape
 
+	return dishes_train, dishes_test, labels_train, labels_test
+		
+def SVM(dishes_train, dishes_test, labels_train, labels_test):
+	clf = SVC()
+	clf.fit(dishes_train, dishes_test)
+	clf.predict(dishes_test)
 # print df
-dishes, labels = ParseAndShuffleData(filename)
 
+dishes, labels = ParseAndShuffleData(filename)
+dishes_train, dishes_test, labels_train, labels_test = PreProcess(dishes, labels)
 # print dishes
 # print labels
-t0 = time()	
-vect = CountVectorizer()
-features = vect.fit_transform(dishes)
-print "vectorize time:", round(time() - t0, 3), "s"
 
 # print vect.get_feature_names()
-print features.shape
