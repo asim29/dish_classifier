d0696432cb216ac824e840a6d67b5763b364d7f2
Asim Waheed <asim.waheed29@gmail.com>
2018-07-11 16:54:50 +0500

    Parse and Shuffle Data
    All data has now been parsed firstly into Pandas DataFrames so they can be shuffled (In case I want to use a K-Fold Cross Validation on it, the labels should be shuffled) and then into np arrays dividing the dishes and the labels, such that it is now ready to be put into a classifier

--- dish_classifier.py
+++ dish_classifier.py
@@ -1,20 +1,49 @@
 import numpy as np
 import pandas as pd 
-import time 
+from time import time 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.utils import shuffle
 
 filename = "test.csv"
 
-df = pd.read_csv(filename)
-df.columns = ['Dish', 'Category']
+def ParseAndShuffleData(filename):
+	df = pd.read_csv(filename)
+	df.columns = ['Dish', 'Category']
 
-category_list = []
+	category_list = []
+	for index, row in df.iterrows():
+		row['Category'] = row['Category'].lower()
+		size = len(row['Category'])
 
-typos = ['f', 'd', 'r', 'e', 'i', 'h']
-for index, row in df.iterrows():
-	if row['Category'] in typos:
-		print index
+	# Deal with spaces at beginning or end
+		if row['Category'][0] == ' ':
+			row['Category'] = row['Category'][1:size]
+			size -= 1
+		if row['Category'][size-1] == ' ':
+			row['Category'] = row['Category'][0:size-1]
 
-	if row['Category'] not in category_list:
-		category_list.append(row['Category'])
+		if row['Category'] not in category_list:
+			category_list.append(row['Category'])
 
-print np.array(category_list)
+	category_list = np.array(category_list)
+
+	df = shuffle(df, random_state = 0)
+	dishes = np.array([x.lower() for x in np.array(df['Dish'])])
+	labels = np.array(df['Category'])
+
+	return dishes, labels
+	# d_cat.to_csv("categories.csv")
+
+
+# print df
+dishes, labels = ParseAndShuffleData(filename)
+
+# print dishes
+# print labels
+t0 = time()	
+vect = CountVectorizer()
+features = vect.fit_transform(dishes)
+print "vectorize time:", round(time() - t0, 3), "s"
+
+# print vect.get_feature_names()
+print features.shape
