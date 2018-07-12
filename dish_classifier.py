853d3133935f10621689a1299b666046f6a17b60
Asim Waheed <asim.waheed29@gmail.com>
2018-07-12 16:46:54 +0500

    Make SVC to fit data
    Made an SVC and found the best parameters using GridSearchCV. Currently using the same set of parameters as found from initial GridSearchCV in order to do more testing. Training Data is inconsistent. Needs to be fixed

--- dish_classifier.py
+++ dish_classifier.py
@@ -5,6 +5,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.utils import shuffle
 from sklearn import model_selection	
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+
 
 filename = "test.csv"
 
@@ -12,7 +15,7 @@ def ParseAndShuffleData(filename):
 	df = pd.read_csv(filename)
 	df.columns = ['Dish', 'Category']
 
-	category_list = []
+	category_list = {}
 	for index, row in df.iterrows():
 		row['Category'] = row['Category'].lower()
 		size = len(row['Category'])
@@ -24,38 +27,61 @@ def ParseAndShuffleData(filename):
 			row['Category'] = row['Category'][0:size-1]
 
 		if row['Category'] not in category_list:
-			category_list.append(row['Category'])
+			category_list[row['Category']] = 0
+		else:
+			category_list[row['Category']] += 1
 
-	category_list = np.array(category_list)
+	# category_list = np.array(category_list)
 
 	df = shuffle(df, random_state = 0)
-	dishes = np.array([x.lower() for x in np.array(df['Dish'])])
+	dishes = np.array(df['Dish'])
 	labels = np.array(df['Category'])
 
+	category_list = pd.DataFrame.from_records([category_list]).transpose()
+	# print category_list
+	category_list.to_csv("categories.csv")
 	return dishes, labels
-	# d_cat.to_csv("categories.csv")
 
 def PreProcess(features, labels):
-	dishes_train, dishes_test, labels_train, labels_test = model_selection.train_test_split(features, labels, test_size = 0.1, random_state = 1)
+	dishes_train, dishes_test, labels_train, labels_test = model_selection.train_test_split(features, labels, test_size = 0.3, random_state = 1)
 	t0 = time()	
-	vect = CountVectorizer()
+	vect = CountVectorizer(stop_words = 'english')
 	dishes_train = vect.fit_transform(dishes_train)
 	dishes_test = vect.transform(dishes_test)
 	print "vectorize time:", round(time() - t0, 3), "s"
 	print dishes_train.shape
 	print dishes_test.shape
-
+	# print vect.get_feature_names()
 	return dishes_train, dishes_test, labels_train, labels_test
+
 		
 def SVM(dishes_train, dishes_test, labels_train, labels_test):
 	clf = SVC()
-	clf.fit(dishes_train, dishes_test)
-	clf.predict(dishes_test)
+
+	
+	print "Fitting the classifier to the training set"
+	t0 = time()
+	param_grid = {
+	         'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+	          'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
+	          }
+	clf = SVC(kernel='rbf', class_weight='balanced', C = 5000, gamma = 0.0005)
+	clf = clf.fit(dishes_train, labels_train)
+	print "done in %0.3fs" % (time() - t0)
+	# print "Best estimator found by grid search:"
+	# print clf.best_estimator_
+
+	pred = clf.predict(dishes_test)
+	print pd.DataFrame(labels_test, pred)
+	print accuracy_score(labels_test, pred)
+
+	return pred
+
+		
 # print df
 
 dishes, labels = ParseAndShuffleData(filename)
 dishes_train, dishes_test, labels_train, labels_test = PreProcess(dishes, labels)
-# print dishes
-# print labels
+predicted = SVM(dishes_train, dishes_test, labels_train, labels_test)
 
 # print vect.get_feature_names()
